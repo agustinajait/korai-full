b9a31f3cca33180662cb998131ce015eaa8e9752
Agustina <agustinajait@gmail.com>
2026-05-12 13:07:57 -0300

    feat: agregar ruta privacidad

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+﻿import { useState, useEffect } from "react";
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -12,6 +12,7 @@ import AdminLogin from "@/pages/admin-login";
 import Prioridades from "@/pages/prioridades";
 import Metas from "@/pages/metas";
 import Confirmar from "@/pages/confirmar";
+import Privacidad from "@/pages/privacidad";
 import { Loader2 } from "lucide-react";
 
 function ProtectedDashboard() {
@@ -35,6 +36,7 @@ function Router() {
       <Route path="/prioridades" component={Prioridades} />
       <Route path="/metas" component={Metas} />
       <Route path="/confirmar" component={Confirmar} />
+      <Route path="/privacidad" component={Privacidad} />
       <Route path="/admin" component={AdminLogin} />
       <Route path="/dashboard" component={ProtectedDashboard} />
       <Route component={NotFound} />
@@ -54,3 +56,5 @@ function App() {
 }
 
 export default App;
+
+
